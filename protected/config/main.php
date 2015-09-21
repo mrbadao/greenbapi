9a29683009e17f18c235614fa3e0aa43939a13a9
hieunc <hieunc@aoi-sys.vn>
2015-09-21 11:15:05 +0700

    imgur

--- protected/config/main.php
+++ protected/config/main.php
@@ -9,6 +9,7 @@ return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'GreenB API Server',
 	'timeZone' => 'Asia/Saigon',
+    'defaultController' => 'authentication/authenticate',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
